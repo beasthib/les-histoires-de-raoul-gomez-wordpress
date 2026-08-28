bd0d032d133e08dfb9f3eae233cfdef268c79958
Beasthib <minnithibault@proton.me>
2026-08-28 21:26:50 +0200

    Aligne les 3 telecabines bout a bout et retire le cable dessine en trop

--- wp-content/themes/raoul-gomez/front-page.php
+++ wp-content/themes/raoul-gomez/front-page.php
@@ -47,9 +47,6 @@ $assets = get_stylesheet_directory_uri() . '/assets';
 		</div>
 
 		<div class="cable-car">
-			<svg class="cable-line" viewBox="0 0 600 220" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-				<line x1="0" y1="150" x2="600" y2="10" stroke="#8a8a8a" stroke-width="2" />
-			</svg>
 			<img class="cabin cabin-1" src="<?php echo esc_url( $assets . '/telecabine.png' ); ?>" alt="" />
 			<img class="cabin cabin-2" src="<?php echo esc_url( $assets . '/telecabine.png' ); ?>" alt="" />
 			<img class="cabin cabin-3" src="<?php echo esc_url( $assets . '/telecabine.png' ); ?>" alt="" />
